1c238283feabe3b3059a63375608b0f2ca6b89f6
Andrei Petro Matos Leal <andrei18l@hotmail.com>
2026-01-14 15:27:10 -0300

    Criação do projeto

---
+++ api/src/test/java/com/andrei/treinamento/api/CofreForteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrei.treinamento.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofreForteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
